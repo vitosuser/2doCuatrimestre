503d8e0147d4e5497a4dd2addb6a38f02eae2fcd
vito <vitoromanloro@gmail.com>
2025-08-17 11:38:16 -0300

    Agregue tp1 ej1 y ej2

--- EJ01/Program.cs
+++ EJ01/Program.cs
@@ -10,6 +10,17 @@ namespace EJ01
     {
         static void Main(string[] args)
         {
+            int a;
+            float b, c;
+
+            a = 5;
+            b = (float) 4.5;
+            c = (float)(a + b);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, c);
+
+            Console.ReadKey();
+
         }
     }
 }

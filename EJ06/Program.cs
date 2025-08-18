8e58f925980dc6b70bbd2b062691d7e25fe65013
vito <vitoromanloro@gmail.com>
2025-08-17 22:48:19 -0300

    agregue ej 6

--- EJ06/Program.cs
+++ EJ06/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EJ05
+namespace EJ06
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
